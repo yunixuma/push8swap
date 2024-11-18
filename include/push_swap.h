be70befd3f0730bd0fbe1393e279cb1fca2b12b1
ykosaka <ykosaka@student.42tokyo.jp>
2024-11-19 06:58:20 +0900

    Modify the header file

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,28 +6,30 @@
 /*   By: ykosaka <ykosaka@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/16 13:03:00 by ykosaka           #+#    #+#             */
-/*   Updated: 2024/03/30 17:42:02 by ykosaka          ###   ########.fr       */
+/*   Updated: 2024/05/05 20:54:20 by ykosaka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+# ifndef BONUS
+#  define BONUS 0
+# endif
+
 # include <unistd.h>
 # include <stdlib.h>
-# include <stdio.h>
+// # include <stdio.h>
 # include <limits.h>
 # include <errno.h>
 # include <stdbool.h>
 # include "push_swap_const.h"
 # include "push_swap_flag.h"
 # include "push_swap_typedef.h"
+# include "push_swap_func.h"
 
-# ifndef BONUS
-#  define BONUS 0
-# endif
 # if BONUS == 0
-#  include "push_swap_func.h"
+#  include "push_swap_func_mandatory.h"
 # else
 #  include "push_swap_func_bonus.h"
 # endif
